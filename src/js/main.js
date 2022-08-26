431dffba30462c4db045bb9ddcbaf194c9966d71
SoinRoma <soinroma26@gmail.com>
2022-08-26 17:24:04 +0500

    - add three buttons for notification, dialog and update

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
-const btn = document.getElementById('btn');
+const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
 
 const notification = window.__TAURI__.notification;
 
 // Test Tauri Notification
-btn.addEventListener('click', () => {
+btn1.addEventListener('click', () => {
 
     let permissionGranted = notification.isPermissionGranted();
         if (!permissionGranted) {
@@ -20,6 +21,18 @@ btn.addEventListener('click', () => {
 btn2.addEventListener('click', () => {
 
     const yes = window.__TAURI__.dialog.ask('Are you sure?', 'Tauri');
+
+})
+
+
+// Test Tauri Update
+btn3.addEventListener('click', () => {
+
+    const update = window.__TAURI__.updater.checkUpdate();
+    console.log(update)
+
+    // window.__TAURI__.process.relaunch();
+
 
 })
 
