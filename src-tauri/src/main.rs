8e62557cdb368e2ed44029d4a5b35479b99d061a
SoinRoma <soinroma26@gmail.com>
2022-10-11 15:48:08 +0500

    - updated main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,57 @@
     windows_subsystem = "windows"
 )]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{SystemTray, SystemTrayMenu, SystemTrayEvent, Manager};
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  let tray_menu = SystemTrayMenu::new();
+  let system_tray = SystemTray::new().with_menu(tray_menu);
+  tauri::Builder::default()
+    .system_tray(system_tray)
+    .on_system_tray_event(|app, event| match event {
+          SystemTrayEvent::LeftClick {position: _,size: _,..} => {
+            let window = app.get_window("main").unwrap();
+            if window.is_visible().unwrap() {
+                //window.show().unwrap();
+                //window.set_focus().unwrap();
+                //.unmaximize().unwrap();
+                               // window.maximize().unwrap();
+                               println!("yes");
+            } else {
+             //  window.show().unwrap();
+            //  window.set_focus().unwrap();
+               // window.unmaximize().unwrap();
+              //  window.maximize().unwrap();
+              println!("no");
+            }
+          }
+          SystemTrayEvent::RightClick {
+            position: _,
+            size: _,
+            ..
+          } => {
+            println!("system tray received a right click");
+          }
+          SystemTrayEvent::MenuItemClick { id, .. } => {
+            match id.as_str() {
+              "quit" => {
+                std::process::exit(0);
+              }
+              "hide" => {
+                let window = app.get_window("main").unwrap();
+                window.hide().unwrap();
+              }
+              _ => {}
+            }
+          }
+          _ => {}
+        })
+    .build(tauri::generate_context!())
+    .expect("error while building tauri application")
+    .run(|_app_handle, event| match event {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+          api.prevent_exit();
+        }
+        _ => {}
+      });
 }
